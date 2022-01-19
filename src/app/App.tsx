547b70fb94e97f93956c2c405d6eb75e3b7bcf43
Almas Barzhaxynov <almas.barzhaxynov@uscg.mil>
2022-01-19 11:50:22 -0500

    updated

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,13 +1,15 @@
 import { FunctionComponent } from "react";
-import { Routes, Route, Link } from "react-router-dom";
+import { Routes } from "react-router-dom";
+import { AppStore } from "../store/AppStore";
 import "./App.css";
 
 export const App: FunctionComponent = () => {
   return (
     <div className="App">
-      <header className="App-header">
-      </header>
-      <Routes></Routes>
+      <AppStore>
+        <header className="App-header"></header>
+        <Routes></Routes>
+      </AppStore>
     </div>
   );
 };
