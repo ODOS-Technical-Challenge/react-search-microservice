a901d27013930d19b9e7de79c6b26159ee4594d3
Almas Barzhaxynov <almas.barzhaxynov@uscg.mil>
2022-01-19 12:17:27 -0500

    updated

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -4,11 +4,13 @@ import { AppStore } from "../store/AppStore";
 import "./App.css";
 
 import { SearchPage } from "../components/search/search.page";
+import { Header } from "../common";
 
 export const App: FunctionComponent = () => {
   return (
     <div className="App">
       <AppStore>
+        <Header />
         <Routes></Routes>
         <SearchPage />
       </AppStore>
--- src/common/header.component.tsx
+++ src/common/header.component.tsx
@@ -1,11 +1,7 @@
 import React, { Fragment, FunctionComponent, ReactNode } from "react";
 import { GovBanner } from "@trussworks/react-uswds";
 
-interface Props {
-  children: ReactNode;
-}
-
-export const Header: FunctionComponent<Props> = ({ children }: Props) => {
+export const Header: FunctionComponent = () => {
   return (
     <Fragment>
       <GovBanner
@@ -13,19 +9,6 @@ export const Header: FunctionComponent<Props> = ({ children }: Props) => {
         tld=".gov"
         aria-label="Official government website"
       />
-      <div
-        style={{
-          height: "4rem",
-          width: "100%",
-          display: "flex",
-          alignItems: "center",
-          boxShadow:
-            "0 2px 2px 0 rgba(0, 0, 0, 0.16), 0 0 0 1px rgba(0, 0, 0, 0.08)",
-        }}
-      >
-        <div style={{ margin: "16px" }}>logo</div>
-        <div>{children}</div>
-      </div>
     </Fragment>
   );
 };
