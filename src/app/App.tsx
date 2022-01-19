ec8cdadcfa1af6e4523069dd0327aaa5c12c1b19
Almas Barzhaxynov <almas.barzhaxynov@uscg.mil>
2022-01-19 12:02:59 -0500

    updated tests

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,4 +1,4 @@
-import { FunctionComponent } from "react";
+import React, { FunctionComponent } from "react";
 import { Routes } from "react-router-dom";
 import { AppStore } from "../store/AppStore";
 import "./App.css";
