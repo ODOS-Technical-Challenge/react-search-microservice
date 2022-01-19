463da7bbb4d5d9c0fdf690c1d1bc3922a439c5e8
Clayton Miller <46973749+cmille62@users.noreply.github.com>
2022-01-19 15:51:05 -0500

    added loading button, modualized components (#7)
    Co-authored-by: Clayton Miller <cmiller@synergybis.com>

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,10 +1,12 @@
 import React, { FunctionComponent } from "react";
-import { Route, Routes } from "react-router-dom";
+import { Navigate, Route, Routes } from "react-router-dom";
 import { AppStore } from "../store/AppStore";
+import { Header } from "../common";
+import { ROUTES } from "../utils";
+
 import "./App.css";
 
 import { SearchPage } from "../components/search/search.page";
-import { Header } from "../common";
 import { RequireAuth } from "../components/auth/require-auth.component";
 import { LoginPage } from "../components/auth/login.page";
 import { AuthStore } from "../store/AuthStore";
@@ -18,13 +20,16 @@ export const App: FunctionComponent = () => {
           <Routes>
             <Route path="/login" element={<LoginPage />} />
             <Route
-              path="/"
+              {...ROUTES.Home}
               element={
                 <RequireAuth>
                   <SearchPage />
                 </RequireAuth>
               }
             />
+
+            {/** Fallback Redirect / Default Page */}
+            <Route path="" element={<Navigate to={ROUTES.Home.path} />} />
           </Routes>
         </AuthStore>
       </AppStore>
